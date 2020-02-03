232b4d469fd89ae0c63dfcb2105680907ac6b2be
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-02-03 22:10:09 +0100

    send and receive data to the server

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at port 3000'));
 app.use(express.static('public')); //to make express display static html files from folder 'public'
+//below adds to the server the ability to parse received data as json
+app.use(express.json({ limit: '1mb' })); //limit specifies maximum size of any body that is coming in
+
+app.post('/api', (request, response) => {
+	const data = request.body;
+
+	//it is required to complete a request - at the minimum we can use 'response.end()'
+	response.json({					
+		status: 'success',
+		latitude: data.lat,
+		longitude: data.lon
+	})
+});
--- public/geolocation.js
+++ public/geolocation.js
@@ -2,10 +2,24 @@
 	if ("geolocation" in navigator) {
 	  	navigator.geolocation.getCurrentPosition(position => {
 		  	const lat = position.coords.latitude,
-		  		long = position.coords.longitude;
+		  		lon = position.coords.longitude;
 
 		  	$('#latitude').text(lat);
-		  	$('#longitude').text(long);
+		  	$('#longitude').text(lon);
+
+		  	const data = { lat, lon },
+		  	options = {
+		  		method: 'POST',
+		  		mode: 'cors',
+		  		headers: {
+		  			'Content-Type': 'application/json'
+		  		},
+		  		body: JSON.stringify(data)
+		  	}
+
+		  	fetch('/api', options).then(response => {
+		  		console.log(response.json());
+		  	});
 		});
 	} else {
 	  console.log("geolocation not available")
